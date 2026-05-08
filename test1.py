55dfcb06ec99d5d42698c261b703eac968b64b9a
Ibrahim Abdelshafy <ibrahim.abdelshafyy@gmail.com>
2026-05-08 19:25:05 +0300

    fix: use current year instead of hardcoded 2026

--- test1.py
+++ test1.py
@@ -1,6 +1,8 @@
+import datetime
+
 name = input("What's Your Name? ")
 year_of_birth = int(input("What's Your Year Of Birth? "))
 
-age = 2026-year_of_birth
+age = datetime.datetime.now().year - year_of_birth
 
 print(f"Hello {name}, you are {age} years old" )
